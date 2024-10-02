426a38005452c3efce091dc255e9a54a3cf57505
ArifkyWildan <muhammadarifkywildan@gmail.com>
2024-10-01 21:05:39 -0700

    first commit

---
+++ app/Http/Controllers/JurusanController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Models\Jurusan;
+use Illuminate\View\View;
+use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Redirect;
+
+class JurusanController extends Controller
+{
+    public function index()
+    {
+        $jurusan = Jurusan::latest()->paginate(10);
+        return view('jurusan.index',compact('jurusan'));
+    }
+
+    public function create()
+    {
+       return view('jurusan.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'jurusan' => 'required|min:5',
+            'detail' => 'required|min:5',
+        ]);
+        $jurusan = jurusan::create([
+            'jurusan' => $request->jurusan,
+            'detail' => $request->detail,
+        ]);
+
+        return redirect()->route('jurusan.index');
+    }
+
+    public function show($id)
+    {
+        $jurusan = Jurusan ::findOrFail($id);
+        return view('jurusan/show',compact('jurusan'));
+    }
+
+}
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,22 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Admin') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12 text-center">
+        <a href="{{route('jurusan.index')}}"> 
+            <button type="button" class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 ml-10">
+            Jurusan
+            </button>
+            </a>
+
+            <a href="{{route('siswa.index')}}"> 
+                <button type="button" class="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+                Siswa
+                </span>
+                </button>
+                </a>
+    </div>
+</x-app-layout>
